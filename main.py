8750cb2f085fa883fd120c7403554adf30c0b1b8
devaansh100 <devaanshgupta100@gmail.com>
2021-08-16 16:17:29 +0530

    Add driver function for training and inferencing

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import argparse
+sys.path.append('./preprocess_text')
+sys.path.append('./fasttext_model')
+
+from create_corpus import create_corpus
+from preprocess_text import preprocess_text
+from create_corpus_fasttext import create_corpus_fasttext
+
+def main(train_csv: str, test_csv: str, model_name: str, remove_html: bool, remove_digits: bool, remove_emoticons: bool, remove_stopwords: bool):
+	print('Creating the training corpus')
+	create_corpus(train_csv, model_name)
+	print('Training corpus created')
+	print('Creating fasttext corpus')
+	create_corpus_fasttext(train_csv, f'dataset/{model_name}_train.txt', f'dataset/{model_name}_train_fasttext.txt', model_name)
+	print('Fasttext corpus created')
+	print('Creating the test corpus')
+	create_corpus(test_csv, model_name, train = False)
+	print('Test corpus created')
+	print('Preprocessing the train and test corpora')
+	# preprocess_text('dataset/train_fasttext.txt','dataset/train_cleaned.txt',remove_html,remove_digits,remove_emoticons,remove_stopwords)
+	# preprocess_text('dataset/test.txt','dataset/test_cleaned.txt',remove_html,remove_digits,remove_emoticons,remove_stopwords)
+	print('Preprocessing completed')
+	os.chdir('fasttext_model/scripts')
+	os.system('./build_fasttext.sh')
+	print('Beginning Training')
+	os.system(f'./train.sh ../../dataset/{model_name}_train_fasttext.txt ../models/{model_name}')
+	print('Training Completed')
+	print('Beginning Inference')
+	os.system(f'./infer.sh ../models/{model_name}.bin ../../dataset/{model_name}_test.txt')
+	print('Inference Completed')
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--train_csv', type = str, default = 'dataset/train.csv', help =  'Path to the training csv file with labels in the last column')
+	parser.add_argument('--test_csv', type = str, default = 'dataset/test.csv', help =  'Path to the testing csv file')
+	parser.add_argument('--model_name', type = str, default = 'classify', help = 'Name of model being trained')
+	parser.add_argument('--remove_html', type = bool, default = True, help = 'Removes html tags from text')
+	parser.add_argument('--remove_digits', type = bool, default = True, help = 'Removes numbers from text')
+	parser.add_argument('--remove_emoticons', type = bool, default = True, help = 'Removes emojis from text')
+	parser.add_argument('--remove_stopwords', type = bool, default = True, help = 'Removes stopwords from text')
+	args = parser.parse_args()
+	main(args.train_csv, args.test_csv, args.model_name, args.remove_html, args.remove_digits, args.remove_emoticons, args.remove_stopwords)
+
